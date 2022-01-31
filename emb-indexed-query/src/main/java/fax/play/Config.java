a4d647ece14742702899d9f4e8bbc484d2be90fc
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2022-01-31 14:51:48 +0100

    Use protostream cache encoding

--- emb-indexed-query/src/main/java/fax/play/Config.java
+++ emb-indexed-query/src/main/java/fax/play/Config.java
@@ -1,7 +1,9 @@
 package fax.play;
 
+import org.infinispan.commons.dataconversion.MediaType;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.cache.IndexStorage;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.manager.EmbeddedCacheManager;
 
@@ -10,13 +12,21 @@ public class Config {
    public static final String CACHE_NAME = "my-cache";
 
    public static EmbeddedCacheManager start() {
-      EmbeddedCacheManager cacheManager = new DefaultCacheManager();
+      GlobalConfigurationBuilder global = new GlobalConfigurationBuilder()
+            .nonClusteredDefault();
+      global.serialization()
+            .addContextInitializer(GameSchema.INSTANCE);
+
+      EmbeddedCacheManager cacheManager = new DefaultCacheManager(global.build());
 
       ConfigurationBuilder builder = new ConfigurationBuilder();
-      builder.indexing()
-            .enable()
-            .storage(IndexStorage.LOCAL_HEAP)
-            .addIndexedEntity(Game.class);
+      builder
+            .encoding()
+               .mediaType(MediaType.APPLICATION_PROTOSTREAM_TYPE)
+            .indexing()
+               .enable()
+               .storage(IndexStorage.LOCAL_HEAP)
+               .addIndexedEntity(Game.class);
 
       cacheManager.createCache(CACHE_NAME, builder.build());
       return cacheManager;
--- emb-indexed-query/src/main/java/fax/play/Game.java
+++ emb-indexed-query/src/main/java/fax/play/Game.java
@@ -3,6 +3,8 @@ package fax.play;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.KeywordField;
+import org.infinispan.protostream.annotations.ProtoFactory;
+import org.infinispan.protostream.annotations.ProtoField;
 
 @Indexed(index = "play")
 public class Game {
@@ -13,8 +15,19 @@ public class Game {
    @FullTextField
    private String description;
 
+   @ProtoFactory
    public Game(String name, String description) {
       this.name = name;
       this.description = description;
+   }
+
+   @ProtoField(1)
+   public String getName() {
+      return name;
+   }
+
+   @ProtoField(2)
+   public String getDescription() {
+      return description;
    }
 }
