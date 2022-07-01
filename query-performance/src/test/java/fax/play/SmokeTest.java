7114524ad94936bf84dd70369f75a7548d40d537
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2022-07-01 17:03:16 +0200

    Perform a query count

--- query-performance/src/test/java/fax/play/SmokeTest.java
+++ query-performance/src/test/java/fax/play/SmokeTest.java
@@ -1,22 +1,41 @@
 package fax.play;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import org.infinispan.client.hotrod.RemoteCache;
-import org.infinispan.client.hotrod.RemoteCacheContainer;
+import org.infinispan.client.hotrod.Search;
+import org.infinispan.query.dsl.Query;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SmokeTest {
 
-   private RemoteCacheContainer cacheManager;
+   private RemoteCache<Object, Shape> cache;
 
-   @Test
-   public void test() {
-      RemoteCache<Object, Shape> cache = Config.setupAll();
-      cacheManager = cache.getRemoteCacheContainer();
+   @BeforeEach
+   public void start() {
+      cache = Config.setupAll();
    }
 
    @AfterEach
    public void end() {
-      cacheManager.stop();
+      if (cache != null) {
+         cache.getRemoteCacheContainer().stop();
+      }
+   }
+
+   @Test
+   public void test() {
+      cache.put("circle", new Shape("circle", 7, Integer.MAX_VALUE, "A circle is a shape consisting ... bla bla bla"));
+      cache.put("square", new Shape("square", 9, 4, "In Euclidean geometry, a square is a regular quadrilateral ... bla bla bla"));
+      cache.put("triangle", new Shape("triangle", 29, 3, "In Euclidean geometry, any three points, when non-collinear ... bla bla bla"));
+
+      Query<?> query = Search.getQueryFactory(cache).create("select count(s.name) from Shape s");
+      List<?> result = query.execute().list();
+
+      assertThat(result).isNotNull();
    }
 }
