735e80235c1be64305f1066dbe47d61e63b98044
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2023-03-30 10:32:59 +0200

    Upgrade the project
    Thanks to tkobayas

--- unit-tests/src/test/java/fax/play/container/InfinispanContainerTest.java
+++ unit-tests/src/test/java/fax/play/container/InfinispanContainerTest.java
@@ -2,27 +2,52 @@ package fax.play.container;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.infinispan.client.hotrod.DefaultTemplate;
 import org.infinispan.client.hotrod.RemoteCache;
-import org.infinispan.test.testcontainers.InfinispanContainer;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.server.test.core.InfinispanContainer;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.junit.jupiter.api.TestInstance;
 
-@Testcontainers
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class InfinispanContainerTest {
 
-   @Container
-   private final static InfinispanContainer INFINISPAN_SERVER = new InfinispanContainer().withCache("mycache");
+   private static final String CACHE_NAME = "my-cache";
+
+   private InfinispanContainer container = new InfinispanContainer();
+   private RemoteCacheManager cacheManager;
+   private RemoteCache<String, String> cache;
+
+   @BeforeAll
+   public void setup() {
+      container.start();
+      cacheManager = container.getRemoteCacheManager();
+      cache = cacheManager.administration().createCache(CACHE_NAME, DefaultTemplate.DIST_SYNC);
+   }
 
    @AfterAll
-   public static void cleanup() {
-      INFINISPAN_SERVER.stop();
+   public void cleanup() {
+      // dispose client resources
+      if (cache != null) {
+         cache.stop(); // this is optional
+      }
+      if (cacheManager != null) {
+         cacheManager.close(); // or stop that is the same
+      }
+
+      // dispose server resources
+      if (container != null) {
+         container.close(); // or stop that is the same
+      }
    }
 
    @Test
    public void test() {
-      RemoteCache<String, String> cache = INFINISPAN_SERVER.getCacheManager().getCache("mycache");
-      assertThat(cache).isNotNull();
+      cache.put("Hello", "World");
+
+      String value = cache.get("Hello");
+      assertThat(value).isEqualTo("World");
    }
 }
