573bebbaa4b92a3b17eb69bb5394992d397da64f
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2023-04-21 17:14:31 +0200

    Simplify circular error reproducers

--- circular-dep-manual/src/main/java/fax/play/model/Company.java
+++ circular-dep-manual/src/main/java/fax/play/model/Company.java
@@ -2,14 +2,11 @@ package fax.play.model;
 
 import java.util.List;
 
-import org.infinispan.protostream.annotations.ProtoField;
-
 public class Company {
 
    private String name;
    private List<Employee> employees;
 
-   @ProtoField(value = 1)
    public String getName() {
       return name;
    }
@@ -18,7 +15,6 @@ public class Company {
       this.name = name;
    }
 
-   @ProtoField(value = 2)
    public List<Employee> getEmployees() {
       return employees;
    }
